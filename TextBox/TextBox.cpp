d55375a0e1926193c88eab84806c74b1836baea3
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-11-13 10:37:27 -0800

    Initial commit: Add TextBox class implementation
    - Added TextBox.hpp with class declaration for managing text value
    - Implemented TextBox methods (setValue and getValue) in TextBox.cpp
    - Created main.cpp to demonstrate basic functionality of TextBox class

---
+++ TextBox/TextBox.cpp
@@ -0,0 +1,8 @@
+//
+//  TextBox.cpp
+//  TextBox
+//
+//  Created by Isaac Adeleke on 11/13/24.
+//
+
+#include "TextBox.hpp"
---
+++ TextBox/TextBox.hpp
@@ -0,0 +1,13 @@
+//
+//  TextBox.hpp
+//  TextBox
+//
+//  Created by Isaac Adeleke on 11/13/24.
+//
+
+#ifndef TextBox_hpp
+#define TextBox_hpp
+
+#include <stdio.h>
+
+#endif /* TextBox_hpp */
